d3f7ff911bd64c3634d28e1b34c858ed783ab659
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-04-18 15:13:49 +0100

    Remove private props, getters/setters, toJSON()

--- src/js/project.js
+++ src/js/project.js
@@ -1,53 +1,10 @@
 export default class Project {
-  #id;
-  #dateCreated;
-  #projectName;
-  #description;
-  #taskList;
-
   constructor({ name, description = "" }) {
-    this.#id = crypto.randomUUID();
-    this.#dateCreated = Date.now();
-    this.#projectName = name;
-    this.#description = description;
-    this.#taskList = [];
-    this.toJSON = function () {
-      return {
-        id: this.#id,
-        dateCreated: this.#dateCreated,
-        name: this.#projectName,
-        description: this.#description,
-        taskList: this.#taskList,
-      };
-    };
-  }
-
-  get id() {
-    return this.#id;
-  }
-
-  get dateCreated() {
-    return this.#dateCreated;
-  }
-
-  get projectName() {
-    return this.#projectName;
-  }
-
-  get description() {
-    return this.#description;
-  }
-
-  get taskList() {
-    return this.#taskList;
-  }
-
-  set projectName(name) {
-    this.#projectName = String(name);
-  }
-
-  set description(text) {
-    this.#description = String(text);
+    this.id = crypto.randomUUID();
+    this.dateCreated = Date.now();
+    this.projectName = name;
+    this.description = description;
+    this.taskList = [];
   }
 
   addTask(task = {}) {
@@ -55,13 +12,13 @@ export default class Project {
       throw new Error(`Invalid argument: expected type 'object', but received type '${typeof task}'.`);
     }
 
-    this.#taskList.push(task);
+    this.taskList.push(task);
   }
 
   removeTask(taskId) {
-    const index = this.#taskList.findIndex((task) => task.id === taskId);
+    const index = this.taskList.findIndex((task) => task.id === taskId);
     if (index !== -1) {
-      this.#taskList.splice(index, 1);
+      this.taskList.splice(index, 1);
     }
   }
 }
