f44343013c9cb1c99e61f86b86b1e466d94b8f4d
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-04-25 11:31:26 +0100

    Add funcs to add/remove tasks to projects

---
+++ src/js/projectTaskConnector.js
@@ -0,0 +1,23 @@
+import { projectsCollection } from "./projectsCollection.js";
+
+export function addTaskToProject(projectID, task) {
+  if (typeof task !== "object") {
+    throw new Error(`Invalid argument: Expected type 'object', received type '${typeof task}'`);
+  }
+
+  const projects = projectsCollection.getProjects();
+
+  projects.forEach((project) => {
+    if (project.id === projectID) {
+      project.addTask(task);
+    }
+  });
+}
+
+export function removeTaskFromProject(taskID) {
+  const projects = projectsCollection.getProjects();
+
+  for (const project of projects) {
+    project.removeTask(taskID);
+  }
+}
