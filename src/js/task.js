3bd5af70a292d2c0599b0b3ee73c64a170049268
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-04-25 10:13:36 +0100

    Add project ID to class Task, update task ID name
    - Project id added to add/remove tasks easier from projects
    - id updated to taskID in class for clarity

--- src/js/task.js
+++ src/js/task.js
@@ -1,6 +1,7 @@
 export default class Task {
-  constructor({ name, description = "", dueDate = "", priority = 0, notes = "" }) {
-    this.id = crypto.randomUUID();
+  constructor({ name, projectID = "", description = "", dueDate = "", priority = 0, notes = "" }) {
+    this.taskID = crypto.randomUUID();
+    this.projectID = projectID;
     this.dateCreated = Date.now();
     this.name = name;
     this.description = description;
