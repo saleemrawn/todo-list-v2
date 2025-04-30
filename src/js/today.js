a50f878204d26eee4d277c055e8d2f555dac057c
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-04-30 11:08:19 +0100

    Add setPageType func to set 'data-page' attr

--- src/js/today.js
+++ src/js/today.js
@@ -1,12 +1,13 @@
 import { lightFormat } from "date-fns";
 import { projectsCollection } from "./projectsCollection.js";
-import { addPageTitleToDOM, addTaskToDOM, resetContentContainer } from "./domController.js";
+import { addPageTitleToDOM, addTaskToDOM, resetContentContainer, setPageType } from "./domController.js";
 
 export default function loadToday() {
   const projects = projectsCollection.getProjects();
   const todayDate = lightFormat(new Date(), "yyyy-MM-dd");
 
   resetContentContainer();
+  setPageType("today");
   addPageTitleToDOM("Today");
 
   for (const project of projects) {
