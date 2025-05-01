8f38d9c30b5538198343145dfcace407c66d13cc
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-05-01 12:00:42 +0100

    Add func to add all projects in arr to storage

--- src/js/storage.js
+++ src/js/storage.js
@@ -4,6 +4,14 @@ export function addProjectToStorage(projectID, project) {
   localStorage.setItem(projectID, JSON.stringify(project));
 }
 
+export function addAllProjectsToStorage() {
+  const projects = projectsCollection.getProjects();
+
+  for (const project of projects) {
+    localStorage.setItem(project.id, JSON.stringify(project));
+  }
+}
+
 export function deleteProjectFromStorage(projectID) {
   localStorage.removeItem(String(projectID));
 }
