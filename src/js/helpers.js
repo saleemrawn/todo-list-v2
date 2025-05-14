acae0f9be63c3e2129441f08c0d853023173cfb5
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-05-14 10:23:46 +0100

    Add func convert date timestamp to formatted str

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,4 +1,11 @@
+import { lightFormat } from "date-fns";
+
 export function findParentElement(element, selector) {
   while ((element = element.parentElement) && !element.matches(selector));
   return element;
+}
+
+export function getFormattedDate(date) {
+  const timestamp = new Date(date);
+  return lightFormat(timestamp, "dd/MM/yyyy");
 }
